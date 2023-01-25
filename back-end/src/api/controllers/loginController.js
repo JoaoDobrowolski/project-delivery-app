4d8f47afe37313881928d0f4ada9f034e5d7d55f
EduMLAlmeida <edualmeidawebdev@gmail.com>
2023-01-25 11:03:17 -0300

    fix - api enviando resposta com status 404 corretamente.

--- back-end/src/api/controllers/loginController.js
+++ back-end/src/api/controllers/loginController.js
@@ -4,20 +4,27 @@ const loginController = {
   login: async (req, res) => {
     const { email, password } = req.body;
     const loginValidation = await loginService.validateLogin(email, password);
-    return res.status(200).json({ loginValidation });
+
+    if (loginValidation.emailValidation.status === 400) {
+      const { status } = loginValidation.emailValidation;
+      const { message } = loginValidation.emailValidation;
+      return res.status(status).json(message);
+    }
+
+    if (loginValidation.passwordValidation.status === 400) {
+      const { status } = loginValidation.passwordValidation;
+      const { message } = loginValidation.passwordValidation;
+      return res.status(status).json(message);
+    }
+
+    if (loginValidation.userValidation.status === 404) {
+      const { status } = loginValidation.userValidation;
+      const { message } = loginValidation.userValidation;
+      return res.status(status).json(message);
+    }
+
+    return res.status(200).json(loginValidation.userValidation);
   },
 };
 
 module.exports = loginController;
-
-// const loginController = {
-//   login: async (req, res) => {
-//     const { email, password } = req.body;
-//     const isValid = loginService.validateLogin(email, password);
-//     if (isValid.message) return res.status(400).json({ message: isValid.message });
-//     const token = await loginService.login(email, password);
-//     if (token.message) return res.status(400).json({ message: token.message });
-//     return res.status(200).json({ token });
-//     return res.status(200).json('controller ok');
-//   },
-// };
--- back-end/src/api/services/loginService.js
+++ back-end/src/api/services/loginService.js
@@ -4,17 +4,30 @@ const db = require('../../database/models');
 const loginService = {
   validateUser: async (email) => {
     const user = await db.User.findOne({ where: { email } });
+
     if (user === null) {
-     return { message: 'User not found' }; 
+      return {
+        status: 404,
+        message: 'User not found',
+      };
     }
-    return {};
+
+    const response = {
+      name: user.name,
+      email: user.email,
+      role: user.role,
+    };
+
+    return response;
   },
   
   validateLogin: async (email, password) => {
     const emailValidation = validations.validateEmail(email);
     const passwordValidation = validations.validatePassword(password);
     const userValidation = await loginService.validateUser(email);
+
     const resp = { emailValidation, passwordValidation, userValidation };
+
     return resp;
   },
 };
