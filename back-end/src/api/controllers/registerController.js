843f6677d6fda343ed2d12fc3cf471ab654d01c0
Daniel Saraiva <danieldsvn@gmail.com>
2023-01-25 12:25:57 -0300

    altera o retorno do service para o controller. Tem que corrigir os casos de status 409 na response

--- back-end/src/api/controllers/registerController.js
+++ back-end/src/api/controllers/registerController.js
@@ -1,10 +1,13 @@
 const registerService = require('../services/registerService');
 
-const registerController = {
-  // Falta criar o usuário no banco de dados e criar situações de não sucesso
+const registerController = {  
   register: async (req, res) => {
     const { name, email, password } = req.body;   
     const register = await registerService.validateRegister(name, email, password);
+    if(register.message) {
+      const { status, message } = register;
+      return res.status(status).json(message);
+    }
     return res.status(201).json(register);
   },
 };
--- back-end/src/api/services/registerService.js
+++ back-end/src/api/services/registerService.js
@@ -3,11 +3,11 @@ const db = require('../../database/models');
 
 const registerService = {
   emailAvailability: async (email) => {
-    const user = await db.User.findOne({ where: { email } });
+    const user = await db.User.findOne({ where: { email } });    
     if (user !== null) {
      return { message: 'This email is not available' }; 
     }
-    return true;
+    return true;    
   },
 
   nameAvailability: async (name) => {
@@ -20,18 +20,25 @@ const registerService = {
   
   validateRegister: async (name, email, password) => {
     const nameValid = validations.validateName(name);
+    if(nameValid.message) return { status: 409, message: nameValid.message}
+
     const emailValid = validations.validateEmail(email);
+    if(emailValid.message) return { status: 409, message: emailValid.message}
+
     const passwordValid = validations.validatePassword(password);
+    if(passwordValid.message) return { status: 409, message: passwordValid.message}
+
     const emailAvailability = await registerService.emailAvailability(email);
-    const nameAvailability = await registerService.nameAvailability(email);
-    const role = 'customer';
-    if (nameValid && emailValid && passwordValid && emailAvailability && nameAvailability) {
-      const newUser = await db.User.create({ name, email, password, role})
-      console.log('newUser --->', newUser);
-      return newUser;
-    }
-    // const resp = { nameValid, emailValid, passwordValid, emailAvailability, nameAvailability };
-    // return resp;
+    if(emailAvailability.message) return { status: 409, message: emailAvailability.message}   
+
+    const nameAvailability = await registerService.nameAvailability(name);
+    if(nameAvailability.message) return { status: 409, message: emailAvailability.message}
+
+    const role = 'customer';    
+    const newUser = await db.User.create({ name, email, password, role})
+    
+    return newUser; 
+    
   },
 };
 
