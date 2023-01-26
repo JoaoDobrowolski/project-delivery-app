8c741268cca09b0e2a0411bad0852f419a6f9718
Daniel Saraiva <danieldsvn@gmail.com>
2023-01-26 11:05:22 -0300

    back-end lint

--- back-end/src/api/app.js
+++ back-end/src/api/app.js
@@ -1,9 +1,10 @@
 const express = require('express');
+const cors = require('cors');
 const routes = require('./routes/routes');
-const cors = require('cors')
+
 const app = express();
 
-app.use(cors())
+app.use(cors());
 app.use(express.json());
 app.use(routes);
 
