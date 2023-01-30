d337a686c587782041330028143b6d43e421d46f
jenifergs <contato.comercialjenifer@gmail.com>
2023-01-30 10:42:01 -0300

    feat: Revisando backend requisito 21

--- back-end/src/api/services/salesService.js
+++ back-end/src/api/services/salesService.js
@@ -1,17 +1,20 @@
 const db = require('../../database/models');
 
 const salesService = {
-  registerProducts: async (saleData) => {
+  registerProducts: async (saleData, saleId) => {
     console.log('teste --> ', saleData);
     saleData.saleProducts.forEach(async (product) => {
       await db.SaleProduct.create({
-        saleId: saleData.userId,
+        saleId: saleId,
         productId: product.productId,
         quantity: product.quantity,
       });
     });
   },
+
   createSale: async (saleData) => {
+   const newDate = new Date();
+    newDate.setUTCDate(newDate.getUTCDate());
     const sale = await db.Sale.create({
       userId: saleData.userId,
       sellerId: saleData.sellerId,
@@ -19,9 +22,10 @@ const salesService = {
       deliveryAddress: saleData.deliveryAddress,
       deliveryNumber: saleData.deliveryNumber,
       status: 'pendente',
+      saleDate: newDate,
     });
 
-    await salesService.registerProducts(saleData);
+    await salesService.registerProducts(saleData, sale.id);
     
     return sale;
   },
