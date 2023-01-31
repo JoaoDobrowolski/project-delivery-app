bbe7220aa1aaa2541e03c0edff55446746887528
Andre Marcos <andrmgc@gmail.com>
2023-01-31 11:32:01 -0300

    adiciona rota customer/oder/:id, cria pagina CustomerOrderDetails

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -5,6 +5,7 @@ import Login from './pages/Login';
 import Register from './pages/Register';
 import ClienteProdutos from './pages/ClienteProdutos';
 import Checkout from './pages/Checkout';
+import CustomerOrderDetails from './pages/CustomerOrderDetails';
 
 export default function App() {
   return (
@@ -15,6 +16,7 @@ export default function App() {
         <Route exact path="/register" component={ Register } />
         <Route exact path="/customer/products" component={ ClienteProdutos } />
         <Route exact path="/customer/checkout" component={ Checkout } />
+        <Route exact path="/customer/orders/:id" component={ CustomerOrderDetails } />
       </Switch>
     </div>
   );
---
+++ front-end/src/pages/CustomerOrderDetails.js
@@ -0,0 +1,138 @@
+import { useHistory } from 'react-router-dom';
+
+import React, { useEffect, useState } from 'react';
+import Navbar from '../components/NavBar';
+
+const placeholderIdCheckout = 1;
+const placeHolderSellerName = 'Fulano Teste';
+
+function CustomerOrderDetails() {
+  const history = useHistory();
+  const [username, setUsername] = useState('');
+  const [products, setProducts] = useState([]);
+
+  const getLocalStorage = () => {
+    const userData = localStorage.getItem('user');
+    const userDataObj = JSON.parse(userData);
+    const saleData = localStorage.getItem('saleProducts');
+    const saleDataObj = JSON.parse(saleData);
+    setProducts(saleDataObj);
+    setUsername(userDataObj.name);
+  };
+  const logout = () => {
+    localStorage.removeItem('user');
+    history.push('/');
+  };
+
+  useEffect(async () => {
+    getLocalStorage();
+  }, []);
+
+  return (
+    <div>
+      <Navbar
+        username={ username }
+        logout={ logout }
+      />
+      <table>
+        <thead>
+          <th
+            data-testid={ `customer_order_details__element-order-details-label-order-
+              ${placeholderIdCheckout}` }
+          >
+            Pedido - placeholder id do pedido
+
+          </th>
+          <th
+            data-testid={ `customer_order_details__element-order-details-label-seller-
+              ${placeHolderSellerName}` }
+          >
+            P. Vend: - placeholder nome do(a) vendedor(a)
+
+          </th>
+          <th
+            data-testid="customer_order_details__element-order-details-label-order-date"
+          >
+            Placeholder - Data
+
+          </th>
+          <th
+            data-testid={ `
+            customer_order_details__element-order-details-label-delivery-status
+              ${placeholderIdCheckout}` }
+          >
+            Status Pedido - placeholder entregue ou não
+
+          </th>
+          <button
+            data-testid={ `
+            customer_order_details__element-order-details-button-delivery-check
+              ` }
+          >
+            Placeholder - Marcar como Entregue
+
+          </button>
+          <tr>
+            <th>Item</th>
+            <th>Descrição</th>
+            <th>Quantidade</th>
+            <th>Valor Unitário</th>
+            <th>Sub-total</th>
+          </tr>
+        </thead>
+        <tbody>
+          {products.map((item, i) => {
+            const { name, price, quantity } = item;
+            return (
+              <tr key={ i }>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-item-number-${i}`
+                  }
+                >
+                  {i + 1}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-name-${i}`
+                  }
+                >
+                  {name}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-quantity-${i}`
+                  }
+                >
+                  {quantity}
+                </td>
+                <td
+                  data-testid={
+                    `customer_checkout__element-order-table-unit-price-${i}`
+                  }
+                >
+                  {price.toFixed(2).replace('.', ',')}
+                </td>
+                <div>
+                  <p
+                    data-testid={
+                      `customer_checkout__element-order-table-sub-total-${i}`
+                    }
+                  >
+                    { (price * quantity).toFixed(2).replace('.', ',')}
+                    {/* { products
+                      .reduce((acc, prod) => acc
+              + (Number(prod.price) * prod.quantity), 0).toFixed(2).replace('.', ',') }
+               */}
+                  </p>
+                </div>
+              </tr>
+            );
+          })}
+        </tbody>
+      </table>
+    </div>
+  );
+}
+
+export default CustomerOrderDetails;
