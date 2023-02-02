1668326e8201d5740b84c4918e0afd7761f770cb
Daniel Saraiva <danieldsvn@gmail.com>
2023-02-02 12:05:16 -0300

    Cria rota POST /admin e faz o uso dela no front

--- back-end/src/api/routes/routes.js
+++ back-end/src/api/routes/routes.js
@@ -5,6 +5,7 @@ const productsController = require('../controllers/productsController');
 const salesController = require('../controllers/salesController');
 const sellersController = require('../controllers/sellersController');
 const saleProductsController = require('../controllers/saleProductsController');
+const adminController = require('../controllers/adminController');
 
 const routes = Router();
 
@@ -17,6 +18,7 @@ routes.post('/sales', salesController.createSale);
 routes.get('/sales/:id', salesController.getUserSales);
 routes.put('/sales/:id', salesController.updateStatus);
 routes.get('/saleProducts/:id', saleProductsController.getSaleProducts);
+routes.post('/admin', adminController.register);
 // app.delete('/user/me', loginController.validateToken, userController.deleteUser);
 
 module.exports = routes;
